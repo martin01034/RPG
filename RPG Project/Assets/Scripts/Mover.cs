7c060d889ce9617dcdde01bc4c3029cea981ef6a
martin01034 <kicktv8@gmail.com>
2020-03-31 23:08:15 +0200

    RayCast

--- RPG Project/Assets/Scripts/Mover.cs
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -6,12 +6,17 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
-   
+
+    Ray lastRay;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100000);
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
