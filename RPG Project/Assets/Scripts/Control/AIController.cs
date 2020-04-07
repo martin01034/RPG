9d5b3089c05e5ebc72e9eed8d2b5f90cd7beb2a2
martin01034 <kicktv8@gmail.com>
2020-04-07 21:08:31 +0200

    Enemy AI completed ;) ; Started First Moment

--- RPG Project/Assets/Scripts/Control/AIController.cs
+++ RPG Project/Assets/Scripts/Control/AIController.cs
@@ -1,6 +1,7 @@
 ﻿using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,9 @@ namespace RPG.Control
     {
         [SerializeField] float chaseDistance = 5f;
         [SerializeField] float suspicionTime = 5f;
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointTolerance = 1f;
+        [SerializeField] float waypointDwellTime = 3f;
         Fighter fighter;
         Health health;
         Mover mover;
@@ -18,6 +22,8 @@ namespace RPG.Control
 
         Vector3 guardPosition;
         float timeSinceLastSawPlayer = Mathf.Infinity;
+        float timeSinceArrivedAtWaypoint = Mathf.Infinity;
+        int currenWaypointIndex = 0;
 
 
         private void Start()
@@ -35,24 +41,55 @@ namespace RPG.Control
 
             if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                timeSinceLastSawPlayer = 0;
                 AttackBehavior();
             }
             else if (timeSinceLastSawPlayer < suspicionTime)
             {
-                //Suspicion State
                 SuspicionBehavior();
             }
             else
             {
-                GuardBehavior();
+                PatrolBehaviour();
             }
-            timeSinceLastSawPlayer += Time.deltaTime;
+            UpdateTimers();
         }
 
-        private void GuardBehavior()
+        private void UpdateTimers()
         {
-            mover.StartMoveAction(guardPosition);
+            timeSinceLastSawPlayer += Time.deltaTime;
+            timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
+        private void PatrolBehaviour()
+        {
+            Vector3 nexPosition = guardPosition;
+            if (patrolPath != null)
+            {
+                if (AtWayPoint())
+                {
+                    timeSinceArrivedAtWaypoint = 0;
+                    CycleWayPoint();
+                }
+                nexPosition = GetCurrentWaypoint();
+            }
+            if (timeSinceArrivedAtWaypoint > waypointDwellTime)
+            {
+                mover.StartMoveAction(nexPosition);
+            }
+        }
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currenWaypointIndex);
+        }
+
+        private void CycleWayPoint()
+        {
+            currenWaypointIndex = patrolPath.GetNextIndex(currenWaypointIndex);
+        }
+
+        private bool AtWayPoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < waypointTolerance;
         }
 
         private void SuspicionBehavior()
@@ -62,6 +99,7 @@ namespace RPG.Control
 
         private void AttackBehavior()
         {
+            timeSinceLastSawPlayer = 0;
             fighter.Attack(player);
         }
 
--- RPG Project/Assets/Scripts/Control/PatrolPath.cs
+++ RPG Project/Assets/Scripts/Control/PatrolPath.cs
@@ -11,9 +11,23 @@ namespace RPG.Control
         {
             for (int i = 0; i < transform.childCount; i++)
             {
+                int j = GetNextIndex(i);
                 Gizmos.color = Color.black;
-                Gizmos.DrawSphere(transform.GetChild(0).position, waypointGizmoRadius);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
+        }
+
+        public int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+                return 0;
+            return i + 1;
+        }
+
+        public Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
